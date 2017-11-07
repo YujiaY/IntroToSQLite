6b6beeb3ea121612b80dd2ca3fcb5cdade7908ff
Yujia Yuan <jack.yujia.yuan@gmail.com>
2017-11-07 15:13:33 +0800

    Tut finished

---
+++ IntroToSQLite/IntroToSQLite/App.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace IntroToSQLite
+{
+    public class App : Application
+    {
+        public App()
+        {
+
+            // The root page of your application
+            MainPage = GetMainPage();
+        }
+
+        public static Page GetMainPage()
+        {
+            var database = new RandomThoughtDatabase();
+
+            return new NavigationPage(new RandomThoughtsPage(database));
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
